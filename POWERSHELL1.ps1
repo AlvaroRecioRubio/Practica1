8cb82e15c93c465501544960f0d53d4500162972
AlvaroRecioRubio <91250827+AlvaroRecioRubio@users.noreply.github.com>
2021-09-24 12:39:19 +0200

    Add files via upload

---
+++ POWERSHELL1.ps1
@@ -0,0 +1,15 @@
+﻿#$binario = Read-host "dime el número binariol"
+#$valor=[convert]::toint32($binario,2)
+#"el número es ..."+$valor
+[convert]::ToInt32("A",16)
+[convert]::Tostring("7",2)
+write-host "-----------------------------------------------------------------------------------"
+write-host "|                                                                                 |"
+write-host "|                                                                                 |"
+write-host "|                                                                                 |"
+write-host "|                                     Menú                                        |"
+write-host "|                                                                                 |"
+write-host "|                                                                                 |"
+write-host "|                                                                                 |"
+write-host "|                                                                                 |"
+write-host "-----------------------------------------------------------------------------------"
